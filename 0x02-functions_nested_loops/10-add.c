82d26162659bef4e0826442ed790f079829ff575
oupa shabangu <oupa.m.shabangu@gmail.com>
2022-06-07 15:26:26 -0700

    prints all natural numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ *
+ * @n1: the first interger
+ *
+ * @n2: the second interger
+ *
+ * Return: interger value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
