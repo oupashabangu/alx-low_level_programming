fb1a2c50a7efa380427624d1f680e35ce2632b34
oupa shabangu <oupa.m.shabangu@gmail.com>
2022-06-09 13:39:30 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9 and end
+ * print_numbers - print 0-9 and end with new line
  *
  * Return: 0-9 and new line after
  */
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		_putchar(i + '0');
