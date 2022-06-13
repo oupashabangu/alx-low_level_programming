46e0e22cce6278e83e7b50dea2364d06ee080a33
oupa shabangu <oupa.m.shabangu@gmail.com>
2022-06-13 04:39:01 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string in reverse
+ * @s: an input string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+
+	while (len)
+		_putchar(s[--len]);
+
+	_putchar('\n');
+}
