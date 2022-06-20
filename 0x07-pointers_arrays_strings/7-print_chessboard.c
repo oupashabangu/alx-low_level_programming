89f7ea8666909ca79ce9f4a47660c295d0de15fc
oupa shabangu <oupa.m.shabangu@gmail.com>
2022-06-20 12:22:18 -0700

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: dimentional array
+ *
+ * return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int x;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (x = 0; x < 8; x++)
+		{
+			_putchar(a[i][x]);
+		}
+		_putchar('\n');
+	}
+}
