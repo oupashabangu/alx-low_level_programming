8fc54087d0aa718ecaed280553a384bd03d7d883
oupa shabangu <oupa.m.shabangu@gmail.com>
2022-06-24 06:44:23 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmtname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv);
+
+	return (0);
+}
