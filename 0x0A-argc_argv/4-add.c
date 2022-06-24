bff324920d8001da5a1a489ce25dcde929221f77
oupa shabangu <oupa.m.shabangu@gmail.com>
2022-06-24 13:19:15 -0700

    add positive

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,52 +1,9 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from a string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (n);
-}
-
-
-/**
- * main - adds two positive number
+ * main - add integers
  * @argc: argument count
  * @argv: argument vector
  *
@@ -54,31 +11,26 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int sum, num, i, j, k;
+	int x, i, j;
 
-	sum = 0;
+	x = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
+			if (isdigit(argv[i][j]) == 0)
 			{
-				puts("Error");
+				printf("Error\n");
 				return (1);
 			}
 		}
 	}
-
-	for (k = 1; k < argc; k++)
+	for (i = 1; i < argc; i++)
 	{
-		num = _atoi(argv[k]);
-		if (num >= 0)
-		{
-			sum += num;
-		}
+		x += atoi(argv[i]);
 	}
+	printf("%d\n", x);
 
-	printf("%d\n", sum);
 	return (0);
 }
