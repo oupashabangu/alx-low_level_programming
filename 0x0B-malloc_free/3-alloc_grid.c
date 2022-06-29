3b21f8fb91cbdbe178385251599d92a86a9a4a1e
oupa shabangu <oupa.m.shabangu@gmail.com>
2022-06-29 12:44:04 -0700

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 		if (arr[i] == NULL)
 		{
 			free(arr);
-			for (j = 0; j <= 0; j++)
+			for (j = 0; j <= i; j++)
 				free(arr[j]);
 			return (NULL);
 		}
