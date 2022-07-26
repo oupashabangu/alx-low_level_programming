51aec303f4d6a1c655f39706379db34da72542c4
oupa shabangu <oupa.m.shabangu@gmail.com>
2022-07-26 08:13:11 -0700

    tread lightly

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - read text and print it to posix
+ * @filename: name of file char
+ * @letters: number of letters to read size_t
+ *
+ * Return: read the file display
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDWR);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	rd = read(fd, buff, letters);
+
+	if (rd == -1)
+	{
+		return (0);
+	}
+
+	wd = write(STDOUT_FILENO, buff, rd);
+
+	if (wd == -1)
+	{
+		return (0);
+	}
+
+	close(fd);
+	free(buff);
+
+	return (wd);
+}
